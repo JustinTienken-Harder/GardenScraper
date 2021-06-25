2de295116bc17232b1d24f8fdab04ac0342c5db8
JustinTienken-Harder <justin.tienken-har13@ncf.edu>
2021-06-25 10:42:13 -0400

    added more support

--- scrape_url.py
+++ scrape_url.py
@@ -10,20 +10,19 @@ def match_class(target):
 
 
 URL = 'http://www.jgarden.org/gardens.asp?ID=306'
+URL2 = 'http://www.jgarden.org/gardens.asp?ID=308'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 spaghetti = soup.find_all(match_class(["smallText"]))[1]
 url = spaghetti.tr.a.get_text(strip=True)
-url = table.b.next.next.a.get_text(strip = True)
 all_b = spaghetti.find_all("b")
-the_tag_filter = {2,4,12}
-tags = [x.next.get_text(strip=True) for index, x in enumerate(all_b) if index not in the_tag_filter and not isinstance(x, NavigableString)]
-['URL:', 'Name:', 'Alternate Name:', 
-'Address:', 'Mailing Address:', 'City:',
- 'State:', 'Postal Code:', 'Country:',
- 'Latitude/Longitude:', 'Weather:', 'Phone:',
-  'Fax:', 'E-Mail:', 'Contact:', 'Designer(s):', 
-  'Contruction Date:', 'Public/Private:', 'Hours:', 
-  'Admission:', 'Added to JGarden:', 'Last Updated:', 
-  'JGarden Description:']
-next_filter = {0, 2, 4, 12} #11
+if "URL" in all_b[0].text:
+    filter = {2,4,12}
+else:
+    filter = {1,3,11}
+tags = [x.get_text(strip=True) for index, x in enumerate(all_b) if index not in filter]
+values = [x.next.next.get_text(strip=True) for index, x in enumerate(all_b) if index not in filter]
+scraped_values = dict(zip(tags, values))
+tags_alt = [x.get_text(strip=True) for x in all_b if  not isinstance(x, NavigableString)]
+tags = [x.get_text(strip=True) for index, x in enumerate(all_b) if index not in the_tag_filter and not isinstance(x, NavigableString)]
+values = [x.next.next.get_text(strip=True) for index, x in enumerate(all_b) if index not in the_tag_filter]
