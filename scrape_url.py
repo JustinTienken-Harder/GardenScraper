336129a7f16735bf06f73780738b7bbe43c0fe48
JustinTienken-Harder <justin.tienken-har13@ncf.edu>
2021-06-25 09:05:04 -0400

    more scraping progress

--- scrape_url.py
+++ scrape_url.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 import requests
 
 def match_class(target):                                                        
@@ -12,5 +13,17 @@ URL = 'http://www.jgarden.org/gardens.asp?ID=306'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 spaghetti = soup.find_all(match_class(["smallText"]))[1]
-table = spaghetti.tr
-
+url = spaghetti.tr.a.get_text(strip=True)
+url = table.b.next.next.a.get_text(strip = True)
+all_b = spaghetti.find_all("b")
+the_tag_filter = {2,4,12}
+tags = [x.next.get_text(strip=True) for index, x in enumerate(all_b) if index not in the_tag_filter and not isinstance(x, NavigableString)]
+['URL:', 'Name:', 'Alternate Name:', 
+'Address:', 'Mailing Address:', 'City:',
+ 'State:', 'Postal Code:', 'Country:',
+ 'Latitude/Longitude:', 'Weather:', 'Phone:',
+  'Fax:', 'E-Mail:', 'Contact:', 'Designer(s):', 
+  'Contruction Date:', 'Public/Private:', 'Hours:', 
+  'Admission:', 'Added to JGarden:', 'Last Updated:', 
+  'JGarden Description:']
+next_filter = {0, 2, 4, 12} #11
